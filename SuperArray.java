97230e04df5f19a601886c2c18cc0f1e107934eb
ElizaHlglh <ewu4@stuy.edu>
2018-10-23 11:53:05 -0400

    SuperArray.java

---
+++ SuperArray.java
@@ -0,0 +1,23 @@
+abstract class SuperArray {
+  private String[] data;
+  private int size;
+
+  public SuperArray(){
+    data = String[10];
+  }
+
+  public void clear(){
+    size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  
+
+}
